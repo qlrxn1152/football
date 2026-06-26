19391501ef5e415747e3c50ef65cc33c7bcff826
qlrxn1152 <qlrxn1152@naver.com>
2026-06-26 20:03:59 +0900

    feat: add Home

--- src/main/java/hoon/football/web/HomeController.java
+++ src/main/java/hoon/football/web/HomeController.java
@@ -1,11 +1,16 @@
 package hoon.football.web;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @Slf4j
-@RequiredArgsConstructor
 public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
 }
