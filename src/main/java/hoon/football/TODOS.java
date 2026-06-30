b7ed62f4468ecfa2240c909870cb6c4b6b908a9b
qlrxn1152 <qlrxn1152@naver.com>
2026-06-30 09:27:19 +0900

    Feat: add LoginInterceptor

--- src/main/java/hoon/football/TODOS.java
+++ src/main/java/hoon/football/TODOS.java
@@ -2,8 +2,8 @@ package hoon.football;
 
 public class TODOS {
 
-    // TODO : @SessionAttribute 사용하는 코드들 null 체크 필요
     // TODO : LoginInterceptor
+    // TODO : @SessionAttribute 사용하는 코드들 null 체크 필요
 
 
 }
---
+++ src/main/java/hoon/football/config/WebConfig.java
@@ -0,0 +1,28 @@
+package hoon.football.config;
+
+import hoon.football.web.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns( // 로그인이 필요없는 페이지
+                        "/",
+                        "/login",
+                        "/members/new",
+                        "/members",
+                        "/teams",
+                        "/css/**",
+                        "/js/**",
+                        "/images/**",
+                        "/favicon.ico",
+                        "/error");
+    }
+}
