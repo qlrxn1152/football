8629bddd8372c2ad0c2cdba7ff0be2e62519618e
qlrxn1152 <qlrxn1152@naver.com>
2026-07-01 16:04:08 +0900

    docs: todos

--- src/main/java/hoon/football/TODOS.java
+++ src/main/java/hoon/football/TODOS.java
@@ -2,10 +2,10 @@ package hoon.football;
 
 public class TODOS {
 
+    // TODO : 홈팀 팀장이 확인해서 이 요청을 수락할지 거절할지 선택할수있게 기능구현.
+    // TODO : TeamMatch -> 테스트코드 작성필요
 
-    // TODO : TeamMatchController 구현 & 기능확인
     // TODO : TeamMatchService -> 경기 결과 입력하는 로직들 구현
-
     // TODO : TeamMatchResult -> 득점자 구현
     // TODO : TeamMatchResult -> 나중에, 경기가 끝나고 일장시간이 지났음에도 결과를 입력하지않으면 어떻게할건지 ..
 }
